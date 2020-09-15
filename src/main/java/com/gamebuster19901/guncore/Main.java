51a9dd99eba76e125281a35fdfc32663cdce22f3
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2020-09-15 18:22:35 -0400

    Version 1.7.2.0

--- src/main/java/com/gamebuster19901/guncore/Main.java
+++ src/main/java/com/gamebuster19901/guncore/Main.java
@@ -28,7 +28,7 @@ import net.minecraftforge.fml.common.Mod;
 public class Main {
 	public static final String MODID = "guncore";
 	public static final String MODNAME = "GunCore";
-	public static final String VERSION = "1.7.1.0 - 1.14.4";
+	public static final String VERSION = "1.7.2.0 - 1.14.4";
 	public static final Logger LOGGER = LogManager.getLogger(MODNAME);
 	public static final ResourceLocation FULL_ID = Resourced.getResourceLocation(MODID, Main.class);
 	private static final Test[] TESTS = Test.getActiveTests();
