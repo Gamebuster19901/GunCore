5c0cf0ea6da5889caa2f0aee7b205264083266e4
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2021-03-27 21:59:44 -0400

    Fabric

---
+++ src/main/java/com/gamebuster19901/guncore/GunCore.java
@@ -0,0 +1,12 @@
+package com.gamebuster19901.guncore;
+
+import net.fabricmc.api.ModInitializer;
+
+public class GunCore implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		
+	}
+	
+}
